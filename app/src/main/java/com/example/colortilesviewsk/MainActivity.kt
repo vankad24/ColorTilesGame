d44b8c4950fa51adba005baa648531dfd4816f67
Ivan Petrushin <ivan.kiel@gmail.com>
2023-11-16 12:43:04 +0800

    first task

--- app/src/main/java/com/example/colortilesviewsk/MainActivity.kt
+++ app/src/main/java/com/example/colortilesviewsk/MainActivity.kt
@@ -5,7 +5,8 @@ import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 
-
+// тип для координат
+data class Coord(val x: Int, val y: Int)
 class MainActivity : AppCompatActivity() {
 
 
@@ -14,6 +15,12 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
     }
 
+    fun getCoordFromString(s: String): Coord {
+        // TODO: реализовать функцию, получающую из
+        // строки вида "12" координаты Coord(1,2)
+        // TODO: сообщать в лог координаты
+        return Coord(0,0)
+    }
     fun changeColor(view: View) {
         val brightColor = resources.getColor(R.color.bright)
         val darkColor = resources.getColor(R.color.dark)
